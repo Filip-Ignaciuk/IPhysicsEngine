8edd870e6c01e7849a1beda6cb98b3b611a01d6e
Filip Ignaciuk <77020855+Filip-Ignaciuk@users.noreply.github.com>
2025-09-07 20:42:59 +0100

    Some work on Vector3

--- include/core.hpp
+++ include/core.hpp
@@ -13,9 +13,24 @@ namespace IPhysicsEngine
         real Magnitude();
         real SquareMagnitude();
         void Normalise();
+        void AddScaledVector(const Vector3& _vector,  real scale);
+        void ComponentProductUpdate(const Vector3& _vector);
+        real ScalarProduct(const Vector3& _vector);
 
         Vector3();
         Vector3(real _x, real _y, real _z);
         ~Vector3();
+
+        Vector3 ComponentProduct(const Vector3& _vector);
+
+
+        void operator*=(const real _value);
+        void operator+=(const Vector3& _vector);
+        void operator-=(const Vector3& _vector);
+
+        Vector3 operator+(const Vector3& _vector);
+        Vector3 operator-(const Vector3& _vector);
+        real operator*(const Vector3& _vector);
+
     };
 }
--- source/core.cpp
+++ source/core.cpp
@@ -1,10 +1,11 @@
 #include "core.hpp"
+#include "precision.hpp"
 
+IPhysicsEngine::real IPhysicsEngine::Vector3::Magnitude(){
+    return real_sqrt(x*x+y*y+z*z);
+}
 
-real IPhysicsEngine::Vector3::Magnitude(){
-    return ((real)0);
-
-real IPhysicsEngine::Vector3::SquareMagnitude(){
+IPhysicsEngine::real IPhysicsEngine::Vector3::SquareMagnitude(){
     return m_x*m_x + m_y * m_y + m_z * m_z;
 };
 
@@ -15,6 +16,22 @@ void IPhysicsEngine::Vector3::Normalise(){
     }
 };
 
+void IPhysicsEngine::Vector3::AddScaledVector(const Vector3& _vector,  real scale){
+    m_x += _vector.m_x * scale;
+    m_y += _vector.m_y * scale;
+    m_z += _vector.m_z * scale;
+};
+
+void IPhysicsEngine::Vector3::ComponentProductUpdate(const Vector3& _vector){
+    m_x = _vector.m_x;
+    m_y = _vector.m_y;
+    m_z = _vector.m_z;
+};
+
+IPhysicsEngine::real IPhysicsEngine::Vector3::ScalarProduct(const Vector3& _vector){
+    return m_x * _vector.m_x + m_y * _vector.m_y + _vector.m_z;
+}
+
 IPhysicsEngine::Vector3::Vector3() : m_x(0), m_y(0), m_z(0){
 };
 
@@ -23,3 +40,39 @@ IPhysicsEngine::Vector3::Vector3(real _x, real _y, real _z) : m_x(_x), m_y(_y), 
 };
 
 IPhysicsEngine::Vector3::~Vector3() = default;
+
+IPhysicsEngine::Vector3 IPhysicsEngine::Vector3::ComponentProduct(const Vector3& _vector){
+    return Vector3(m_x * _vector.m_x, m_y * _vector.m_y, m_z * _vector.m_z);
+}
+
+
+void IPhysicsEngine::Vector3::operator*=(const real _value){
+    m_x *= _value;
+    m_y *= _value;
+    m_z *= _value;
+}
+
+void IPhysicsEngine::Vector3::operator+=(const Vector3& _vector){
+    m_x += _vector.m_x;
+    m_y += _vector.m_y;
+    m_z += _vector.m_z;
+}
+
+void IPhysicsEngine::Vector3::operator-=(const Vector3& _vector){
+    m_x -= _vector.m_x;
+    m_y -= _vector.m_y;
+    m_z -= _vector.m_z;
+}
+
+IPhysicsEngine::Vector3 IPhysicsEngine::Vector3::operator+(const Vector3& _vector){
+    return Vector3(m_x + _vector.m_x, m_y + _vector.m_y, m_z + _vector.m_z);
+}
+
+IPhysicsEngine::Vector3 IPhysicsEngine::Vector3::operator-(const Vector3& _vector){
+    return Vector3(m_x - _vector.m_x, m_y - _vector.m_y, m_z - _vector.m_z);
+}
+
+IPhysicsEngine::real IPhysicsEngine::Vector3::operator*(const Vector3& _vector){
+    return m_x * _vector.m_x + m_y * _vector.m_y + m_z * _vector.m_z;
+}
+
