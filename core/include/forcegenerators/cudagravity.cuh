802b22f3d71e7247bf8fb8e04ff8c6243d0e6e37
Filip Ignaciuk <Ignaciuk.Filip@protonmail.com>
2026-09-07 10:31:39 +0100

    Work on O(n^2) cuda implementation

--- core/include/forcegenerators/cudagravity.cuh
+++ core/include/forcegenerators/cudagravity.cuh
@@ -10,6 +10,8 @@ namespace IPhysics {
         void AddObject(IPhysics::Object* _object) override;
         void RemoveObject(IPhysics::Object* _object) override;
         void UpdateForce(IPhysics::RigidBody* _rigidBody, IPhysics::real _duration) override;
+    private:
+        __global__ void vecAdd(float* A, float* B, float* C);
     };
 }
 
